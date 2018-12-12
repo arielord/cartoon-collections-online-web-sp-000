84288a4e0fd38c30704dc882d7a8dcdb30cf7a7e
Ariel <arielord@buffalo.edu>
2018-12-12 21:17:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index {|dwarf, index| puts "#{index+1}.#{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect |calls| do
+    calls.capitalize
+    calls += "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
